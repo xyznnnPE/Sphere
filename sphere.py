3a7f38e9019fb4a7aabf35c1bfea1ffdbaafaeb8
thepipboy <3476647793@qq.com>
2025-03-06 18:58:36 +0800

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -16,10 +16,10 @@ class spacetime():
          this.Z = Radius * theta * fai
     def time(x,y,z,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12):
         return [
-            a * sin(x + t1) + b * cos(x - t1) + c * sin(y + t2) + d * cos(y - t2) + e * sin(z + t3) + f * cos(z - t3),       #t123
-            a * asin(x + t4) + b * acos(x - t4) + c * asin(y + t5) + d * acos(y - t5) + e * asin(z + t6) + f * acos(z - t6), #t456
-            a * sinh(x + t7) + b * cosh(x - t7) + c * sinh(y + t8) + d * cosh(y - t8) + e * sinh(z + t9) + f * cosh(z - t9), #t789
-            a * tan(x + t10) + b * atan(x - t10) + c * tan(y + t11) + d * atan(y - t11) + e * tan(z + t12) + f * atan(z - t12) #t101112
+            a * math.sin(x + t1) + b * math.cos(x - t1) + c * math.sin(y + t2) + d * math.cos(y - t2) + e * math.sin(z + t3) + f * math.cos(z - t3),       #t123
+            a * math.asin(x + t4) + b * math.acos(x - t4) + c * math.asin(y + t5) + d * math.acos(y - t5) + e * math.asin(z + t6) + f * math.acos(z - t6), #t456
+            a * math.sinh(x + t7) + b * math.cosh(x - t7) + c * math.sinh(y + t8) + d * math.cosh(y - t8) + e * math.sinh(z + t9) + f * math.cosh(z - t9), #t789
+            a * math.tan(x + t10) + b * math.atan(x - t10) + c * math.tan(y + t11) + d * math.atan(y - t11) + e * math.tan(z + t12) + f * math.atan(z - t12) #t101112
         ]
 
 class wiki():
