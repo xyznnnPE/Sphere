dad3a0d4de8860f8e29c64bfd078ac692d7e3a60
thepipboy <3476647793@qq.com>
2025-05-06 21:05:40 +0800

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -14,12 +14,12 @@ class spacetime():
          this.X = Radius * math.cos(theta)
          this.Y = Radius * math.sin(fai)
          this.Z = Radius * math.cos(theta) * math.cos(fai)
-    def time(x,y,z,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12):
+    def time(x,y,z,n):
         return [
-            a * math.sin(x + t1) + b * math.cos(x - t1) + c * math.sin(y + t2) + d * math.cos(y - t2) + e * math.sin(z + t3) + f * math.cos(z - t3),       #t123
-            a * math.asin(x + t4) + b * math.acos(x - t4) + c * math.asin(y + t5) + d * math.acos(y - t5) + e * math.asin(z + t6) + f * math.acos(z - t6), #t456
-            a * math.sinh(x + t7) + b * math.cosh(x - t7) + c * math.sinh(y + t8) + d * math.cosh(y - t8) + e * math.sinh(z + t9) + f * math.cosh(z - t9), #t789
-            a * math.tan(x + t10) + b * math.atan(x - t10) + c * math.tan(y + t11) + d * math.atan(y - t11) + e * math.tan(z + t12) + f * math.atan(z - t12) #t101112
+            a * math.sin(x + n) + b * math.cos(x - n) + c * math.sin(y + n) + d * math.cos(y - n) + e * math.sin(z + n) + f * math.cos(z - n),       
+            a * math.asin(x + n) + b * math.acos(x - n) + c * math.asin(y + n) + d * math.acos(y - n) + e * math.asin(z + n) + f * math.acos(z - n),
+            a * math.sinh(x + n) + b * math.cosh(x - n) + c * math.sinh(y + n) + d * math.cosh(y - n) + e * math.sinh(z + n) + f * math.cosh(z - n),
+            a * math.tan(x + n) + b * math.atan(x - n) + c * math.tan(y + n) + d * math.atan(y - n) + e * math.tan(z + n) + f * math.atan(z - n)
         ]
 
 class wiki():
@@ -39,16 +39,9 @@ class wiki():
         self.e = Dodecahedron
         self.f = Icosahedron
 
-        return t1t2[a + b + c + d + e + f]
-        return t3t4[a - b - c - d - e - f]
-        return t5t6[a * b * c * d * e * f]
-        return t7t8[a / b / c / d / e / f]
-        return t9t10[a % b % c % d % e % f]
-        return t11t12[a : b : c : d : e : f]
-        
-class loop():
-    def XYZ():
-        for x in range(2*2*2*2):
-        for y in range(2*2*2*2):
-        for z in range(2*2*2*2):
-            
+        return [a + b + c + d + e + f]
+        return [a - b - c - d - e - f]
+        return [a * b * c * d * e * f]
+        return [a / b / c / d / e / f]
+        return [a % b % c % d % e % f]
+        return [a : b : c : d : e : f]          
