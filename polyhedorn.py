c3d4c9e6c4e4a4a09919b1d44a640d42de1c75ba
thepipboy <3476647793@qq.com>
2025-06-09 09:09:34 +0800

    Create polyhedorn.py

---
+++ polyhedorn.py
@@ -0,0 +1,17 @@
+
+class wiki():
+    def polyhedron(Vertex,Edges,Faces):
+        self.Vertex = [V]
+        self.Edges = [E]
+        self.Faces = [F]
+        return V - E + F == 2
+
+    def abcdef(Tetrahedron,Hexhedron,Octahedron,Dodecahedron,Icosahedron):
+        this.dot = 0
+        this.line = 1
+        self.a = dotline
+        self.b = Tetrahedron
+        self.c = Hexhedron
+        self.d = Octahedron
+        self.e = Dodecahedron
+        self.f = Icosahedron      
