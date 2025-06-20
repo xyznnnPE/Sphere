8fe9188554e65b54bb63799244b5e461e7e3b1e7
thepipboy <3476647793@qq.com>
2025-06-20 09:07:30 +0800

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -10,14 +10,14 @@ class equation():
          e = 5/7
          f = 6/7
    def  x(a,b,c,d,e,f,n):
-        x + n = a * n^5 + b * n^4 + c * n^3 + d * n^2 + e * n + f
-        x - n = a * n^4 + b * n^3 + c * n^2 + d * n + e * n^(-1) + f
+        x + n == a * n^5 + b * n^4 + c * n^3 + d * n^2 + e * n + f
+        x - n == a * n^4 + b * n^3 + c * n^2 + d * n + e * n^(-1) + f
    def  y(a,b,c,d,e,f,n):
-        y + n = a * n^3 + b * n^2 + c * n + d * n^(-1) + e * n^(-2) + f
-        y - n = a * n^2 + b * n + c * n^(-1) + d * n^(-2) + e * n^(-3) + f
+        y + n == a * n^3 + b * n^2 + c * n + d * n^(-1) + e * n^(-2) + f
+        y - n == a * n^2 + b * n + c * n^(-1) + d * n^(-2) + e * n^(-3) + f
    def  z(a,b,c,d,e,f,n):      
-        z + n = a * n + b * n^(-1) + c * n^(-2) + d * n^(-3) + e * n^(-4) + f
-        z - n = a * n^(-1) + b * n^(-2) + c * n^(-3) + d * n^(-2) + e * n^(-5) + f
+        z + n == a * n + b * n^(-1) + c * n^(-2) + d * n^(-3) + e * n^(-4) + f
+        z - n == a * n^(-1) + b * n^(-2) + c * n^(-3) + d * n^(-2) + e * n^(-5) + f
 
 class spacetime():
     def Hexadecimal(A,B,C,D,E,F):
