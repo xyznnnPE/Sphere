f6290ca5d0211946bb7d26eab584bf2eda4d0510
xyznnn <3476647793@qq.com>
2025-10-07 16:32:39 +0800

    Update MineCraft.py

--- MineCraft.py
+++ MineCraft.py
@@ -8,19 +8,13 @@ TICKS_PER_SEC = 60
 SECTOR_SIZE = 16
 
 def xyznnn(x, y, z, n):
-    return [     
-        # Right face (x+n)
-        x+n,y-n,z+n,  x+n,y-n,z-n,  x+n,y+n,z-n,  x+n,y+n,z+n,
-        # Left face (x-n)
-        x-n,y-n,z-n,  x-n,y-n,z+n,  x-n,y+n,z+n,  x-n,y+n,z-n,
-        # Top face (y+n)
-        x-n,y+n,z-n,  x-n,y+n,z+n,  x+n,y+n,z+n,  x+n,y+n,z-n,
-        # Bottom face (y-n)
-        x-n,y-n,z-n,  x+n,y-n,z-n,  x+n,y-n,z+n,  x-n,y-n,z+n,
-        # Front face (z+n)
-        x-n,y-n,z+n,  x+n,y-n,z+n,  x+n,y+n,z+n,  x-n,y+n,z+n,
-        # Back face (z-n)
-        x-n,y-n,z-n,  x-n,y+n,z-n,  x+n,y+n,z-n,  x+n,y-n,z-n,
+    return [ 
+        x+n,y-n,z+n,  x+n,y-n,z-n,  x+n,y+n,z-n,  x+n,y+n,z+n, # Right face (x+n)
+        x-n,y-n,z-n,  x-n,y-n,z+n,  x-n,y+n,z+n,  x-n,y+n,z-n, # Left face (x-n)
+        x-n,y+n,z-n,  x-n,y+n,z+n,  x+n,y+n,z+n,  x+n,y+n,z-n, # Top face (y+n)
+        x-n,y-n,z-n,  x+n,y-n,z-n,  x+n,y-n,z+n,  x-n,y-n,z+n, # Bottom face (y-n)
+        x-n,y-n,z+n,  x+n,y-n,z+n,  x+n,y+n,z+n,  x-n,y+n,z+n, # Front face (z+n)
+        x-n,y-n,z-n,  x-n,y+n,z-n,  x+n,y+n,z-n,  x+n,y-n,z-n, # Back face (z-n)
     ]
 
 def tex_coord(x, y, n=4):
