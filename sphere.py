ea588d13e58b947e66cd7e94330293be5ed44d26
xyznnn <3476647793@qq.com>
2025-10-07 17:37:55 +0800

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -32,5 +32,5 @@ class spacetime():
             A * sin(X + N) + B * cos(X - N) + C * sin(Y + N) + D * cos(Y - N) + E * sin(Z + N) + F * cos(Z - N),       
             A * asin(X + N) + B * acos(X - N) + C * asin(Y + N) + D * acos(Y - N) + E * asin(Z + N) + F * acos(Z - N),
             A * sinh(X + N) + B * cosh(X - N) + C * sinh(Y + N) + D * cosh(Y - N) + E * sinh(Z + N) + F * cosh(Z - N),
-            A * tan(X + N) + B * atan(X - N) + C * tan(Y + N) + D * atan(Y - N) + E * tan(Z + N) + F * atan(Z - N)
-        ] 
+            A * asinh(X + N) + B * acosh(X - N) + C * asinh(Y + N) + D * acosh(Y - N) + E * asinh(Z + N) + F * acosh(Z - N)
+               ] 
