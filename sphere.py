88c76cd570bf131d54ab8bbbd51ec74d277e257e
xyznnn <3476647793@qq.com>
2025-10-07 16:29:51 +0800

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -3,12 +3,7 @@ import math
 
 class equation():
    def Hexadecimal(a,b,c,d,e,f):
-         a = 1/7
-         b = 2/7
-         c = 3/7
-         d = 4/7
-         e = 5/7
-         f = 6/7
+         a = 1/7,b = 2/7,c = 3/7,d = 4/7,e = 5/7,f = 6/7
    def  x(a,b,c,d,e,f,n):
         x + n == a * n^5 + b * n^4 + c * n^3 + d * n^2 + e * n + f
         x - n == a * n^4 + b * n^3 + c * n^2 + d * n + e * n^(-1) + f
@@ -20,6 +15,7 @@ class equation():
         z - n == a * n^(-1) + b * n^(-2) + c * n^(-3) + d * n^(-2) + e * n^(-5) + f
 
 class spacetime():
+   
     def Hexadecimal(A,B,C,D,E,F):
          A = "00001011"
          B = "00001100"
